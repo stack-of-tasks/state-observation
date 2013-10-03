7213f4af046db4cc78a5d85d7af78c1344ababf7
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2013-10-03 16:01:10 +0200

    Renove the definition of discrete-time matrix

--- src/observer-base.cpp
+++ src/observer-base.cpp
@@ -3,18 +3,6 @@
 namespace stateObservation
 {
 
-    DiscreteTimeMatrix::DiscreteTimeMatrix(const Matrix& v,unsigned k):
-            isSet_(true),
-            k_(k),
-            v_(v)
-    {
-    }
-
-    DiscreteTimeMatrix::DiscreteTimeMatrix():
-            isSet_(false),
-            k_(0)
-    {
-    }
 
 
     void ObserverBase::reset()
