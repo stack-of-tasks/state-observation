926e17ee82a26b6cf61903212f99572ccbc57fb4
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2013-10-10 19:05:38 +0200

    rearrange the test for the IMU

--- unit-testings/imu-test.cpp
+++ unit-testings/imu-test.cpp
@@ -14,107 +14,109 @@ int test()
     const unsigned kmax=3000;
     const double dt=1e-3;
 
-    IMUDynamicalSystem imu;
+    const unsigned stateSize=18;
+    const unsigned measurementSize=6;
+    const unsigned inputSize=6;
 
-    DynamicalSystemSimulator sym;
-
-    //GaussianWhiteNoise vk(imu.getMeasurementSize());
-
-    sym.setDynamicsFunctor(&imu);
-
-    Vector x0=Vector::Zero(imu.getStateSize(),1);
-
-    sym.setState(x0,0);
-
+    DiscreteTimeArray x;
+    DiscreteTimeArray y;
     DiscreteTimeArray u;
 
-    for (int i=0;i<kmax/10;++i)
+    Matrix r;
+    Matrix q;
+
     {
-        Vector uk=Vector::Zero(imu.getInputSize(),1);
+        IMUDynamicalSystem imu;
 
-        uk[0]=0.4 * sin(M_PI/10*i);
-        uk[1]=0.6 * sin(M_PI/12*i);
-        uk[2]=0.2 * sin(M_PI/5*i);
+        GaussianWhiteNoise processNoise(imu.getStateSize());
+        Matrix q1=Matrix::Identity(stateSize,stateSize)*0.01;
+        processNoise.setStandardDeviation(q1);
+        imu.setProcessNoise( & processNoise );
+        q=q1*q1.transpose();
 
-        uk[3]=10  * sin(M_PI/12*i);
-        uk[4]=0.07  * sin(M_PI/15*i);
-        uk[5]=0.05 * sin(M_PI/5*i);
+        GaussianWhiteNoise MeasurementNoise(imu.getMeasurementSize());
+        Matrix r1=Matrix::Identity(measurementSize,measurementSize)*0.01;
+        MeasurementNoise.setStandardDeviation(r1);
+        imu.setMeasurementNoise( & MeasurementNoise );
+        r=r1*r1.transpose();
 
-        for (int j=0;j<10;++j)
+        DynamicalSystemSimulator sym;
+        sym.setDynamicsFunctor(&imu);
+
+        Vector x0=Vector::Zero(imu.getStateSize(),1);
+
+        sym.setState(x0,0);
+
+        for (int i=0;i<kmax/10;++i)
         {
-            u.pushBack(uk,i*10+j);
-        }
-        sym.setInput(uk,10*i);
+            Vector uk=Vector::Zero(imu.getInputSize(),1);
 
+            uk[0]=0.4 * sin(M_PI/10*i);
+            uk[1]=0.6 * sin(M_PI/12*i);
+            uk[2]=0.2 * sin(M_PI/5*i);
+
+            uk[3]=10  * sin(M_PI/12*i);
+            uk[4]=0.07  * sin(M_PI/15*i);
+            uk[5]=0.05 * sin(M_PI/5*i);
+
+            for (int j=0;j<10;++j)
+            {
+                u.pushBack(uk,i*10+j);
+            }
+            sym.setInput(uk,10*i);
+
+        }
+
+        imu.setSamplingPeriod(dt);
+
+        sym.simulateDynamicsTo(kmax+1);
+
+        y = sym.getMeasurementArray(1,kmax);
+        x = sym.getStateArray(1,kmax);
     }
 
-    imu.setSamplingPeriod(dt);
-
-    sym.simulateDynamicsTo(kmax+1);
-
-    DiscreteTimeArray y = sym.getMeasurementArray(1,kmax);
-    DiscreteTimeArray x = sym.getStateArray(1,kmax);
-
-    std::ofstream f;
-
-    f.open("trajectory.dat");
+    ExtendedKalmanFilter filter(stateSize, measurementSize, measurementSize, false);
 
     IMUDynamicalSystem imuFunctor;
-
     imuFunctor.setSamplingPeriod(dt);
-
-    imuFunctor=imu;
-
-    ExtendedKalmanFilter filter(imuFunctor.getStateSize(), imuFunctor.getMeasurementSize(),
-                                imuFunctor.getMeasurementSize(), false);
-
     filter.setFunctor(& imuFunctor);
 
-    Vector xh0=filter.stateVectorRandom()*10;
-
+    Vector xh0=filter.stateVectorRandom()*3.14;
+    xh0[9]=3.14;
     filter.setState(xh0,0);
 
     Matrix p=filter.getPmatrixZero();
-
     for (unsigned i=0;i<filter.getStateSize();++i)
     {
         p(i,i)=xh0[i];
     }
     p=p*p.transpose();
-
     filter.setStateCovariance(p);
 
-    Matrix r1=filter.getRmatrixIdentity()*0.0001;
-
-    Matrix q1=filter.getQmatrixZero();
-
-    Matrix r=r1*r1.transpose();
-    Matrix q=q1*q1.transpose();
+    ///set initial input
+    filter.setInput(u[0],0);
 
     ///set the covariance matrices for the extended Kalman filter
     filter.setR(r);
     filter.setQ(q);
 
-    ///set initial input
-    filter.setInput(u[0],0);
-
     ///set the derivation step for the finite difference method
     Vector dx=filter.stateVectorConstant(1)*1e-8;
-
 
     DiscreteTimeArray xh;
     xh.pushBack(xh0,0);
 
+    std::ofstream f;
+    f.open("trajectory.dat");
+
     for (int i=y.getFirstTime();i<=y.getLastTime();++i)
     {
         filter.setMeasurement(y[i],i);
-        if (i<y.getLastTime())
+        if (i<x.getLastTime())
             filter.setInput(u[i],i);
 
         Matrix a=filter.getAMatrixFD(dx);
         Matrix c= filter.getCMatrixFD(dx);
-
-        //std::cout << a << std::endl << c << std::endl;
 
         filter.setA(a);
         filter.setC(c);
@@ -131,6 +133,7 @@ int test()
             else
                 R = Matrix3::Identity();
             g=R.transpose()*Vector3::UnitZ();
+            g.normalize();
         }
 
         Vector3 gh;
@@ -143,10 +146,11 @@ int test()
             else
                 Rh = Matrix3::Identity();
             gh=Rh.transpose()*Vector3::UnitZ();
+            gh.normalize();
         }
 
 
-        f << i<< " \t "<< g.transpose()-gh.transpose()<< " \t\t\t "
+        f << i<< " \t "<< acos(double(g.transpose()*gh)) * 180 / M_PI << " \t\t\t "
         << g.transpose() << " \t\t\t " << gh.transpose() << std::endl;
 
         //std::getchar();
