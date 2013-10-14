da64603066ecf0e9306f3451e5704a9c149b84d7
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2013-10-14 18:25:00 +0200

    Add an examples folder for some implementations, first implementation is the attitude estimation using EKFs on IMU measurements

---
+++ include/state-observation/examples/imu-attitude-trajectory-reconstruction.hpp
@@ -0,0 +1,107 @@
+/**
+ * \file      imu-attitude-trajectory-reconstruction.hpp
+ * \author    Mehdi Benallegue
+ * \date       2013
+ * \brief      Gives an implementation of attitude estimation for IMU reconstruction
+ *             with or without given input. The source is in a file
+ *             imu-attitude-trajectory-reconstruction.hxx
+ *
+ * \details
+ *
+ *
+ */
+
+#ifndef IMUATTITUDETRAJECTORYRECONTRUCTIONHPP
+#define IMUATTITUDETRAJECTORYRECONTRUCTIONHPP
+
+
+#include <state-observation/dynamical-system/imu-dynamical-system.hpp>
+#include <state-observation/dynamical-system/dynamical-system-simulator.hpp>
+#include <state-observation/observer/extended-kalman-filter.hpp>
+#include <state-observation/tools/miscellaneous-algorithms.hpp>
+
+namespace stateObservation
+{
+    namespace examples
+    {
+
+
+        /*! \fn DiscreteTimeArray imuAttitudeTrajectoryReconstruction(
+         *   const DiscreteTimeArray & y,
+         *   const DiscreteTimeArray & u,
+         *   const Vector & xh0,
+         *   const Matrix & p,
+         *   const Matrix & q,
+         *   const Matrix & r,
+         *   double dt);
+         *
+         *  \brief Provides the estimation of the state (mostly attitude) of an
+         *         IMU, given the measurements of the IMU and the input
+         *         (which provides the acceleration/jerk). This method uses
+         *         extended Kalman filtering, we need to provide it with an
+         *         initial guess, a covariance matrix of this initial guess
+         *         and covariance matrices of the state perturbations and
+         *         measurement noises.
+         *
+         *
+         *  \param y IMU measurements
+         *  \param u the inputs of the dynamical system
+         *  \param xh0 an initial guess of the state
+         *  \param p the covariance matrix of the error of the initial guess
+         *  \param q the covariance matrix of the process noise (state perturbation)
+         *  \param r the covariance matrix of the measurement noise
+         *  \param dt the time discretization period
+         */
+
+        DiscreteTimeArray imuAttitudeTrajectoryReconstruction(
+            const DiscreteTimeArray & y,
+            const DiscreteTimeArray & u,
+            const Vector & xh0,
+            const Matrix & p,
+            const Matrix & q,
+            const Matrix & r,
+            double dt);
+
+
+        /*! \fn DiscreteTimeArray imuAttitudeTrajectoryReconstruction(
+         *   const DiscreteTimeArray & y,
+         *   const Vector & xh0,
+         *   const Matrix & p,
+         *   const Matrix & q,
+         *   const Matrix & r,
+         *   double dt);
+         *
+         *  \brief Provides the estimation of the state (mostly attitude) of an
+         *         IMU, given the measurements of the IMU without knowing the input.
+         *         The input is assumed to be zero over the observation. This method uses
+         *         extended Kalman filtering, we need to provide it with an
+         *         initial guess, a covariance matrix of this initial guess
+         *         and covariance matrices of the state perturbations and
+         *         measurement noises.
+         *
+         *
+         *  \param y IMU measurements
+         *  \param xh0 an initial guess of the state
+         *  \param p the covariance matrix of the error of the initial guess
+         *  \param q the covariance matrix of the process noise (state perturbation)
+         *  \param r the covariance matrix of the measurement noise
+         *  \param dt the time discretization period
+         */
+        DiscreteTimeArray imuAttitudeTrajectoryReconstruction(
+            const DiscreteTimeArray & y,
+            const Vector & xh0,
+            const Matrix & p,
+            const Matrix & q,
+            const Matrix & r,
+            double dt);
+
+
+
+
+#include <state-observation/examples/imu-attitude-trajectory-reconstruction.hxx>
+
+    }
+
+}
+
+#endif //IMUATTITUDETRAJECTORYRECONTRUCTIONHPP
---
+++ include/state-observation/examples/imu-attitude-trajectory-reconstruction.hxx
@@ -0,0 +1,100 @@
+DiscreteTimeArray imuAttitudeTrajectoryReconstruction
+    (
+    const DiscreteTimeArray & y,
+    const DiscreteTimeArray & u,
+    const Vector & xh0,
+    const Matrix & p,
+    const Matrix & q,
+    const Matrix & r,
+    double dt)
+{
+    ///Sizes of the states for the state, the measurement, and the input vector
+    const unsigned stateSize=18;
+    const unsigned measurementSize=6;
+    const unsigned inputSize=6;
+
+    ///initialization of the extended Kalman filter
+    ExtendedKalmanFilter filter(stateSize, measurementSize, measurementSize, false);
+
+    ///initalization of the functor
+    IMUDynamicalSystem imuFunctor;
+    imuFunctor.setSamplingPeriod(dt);
+    filter.setFunctor(& imuFunctor);
+
+    ///the initalization of the estimation of the initial state
+    filter.setState(xh0,y.getFirstTime()-1);
+
+    ///computation and initialization of the covariance matrix of the initial state
+    filter.setStateCovariance(p);
+
+    ///set initial input
+    filter.setInput(u[y.getFirstTime()-1],y.getFirstTime()-1);
+
+    ///The covariance matrix of the process noise and the measurement noise
+    /// for the extended Kalman filter
+    filter.setR(r);
+    filter.setQ(q);
+
+    ///set the derivation step for the finite difference method
+    Vector dx=filter.stateVectorConstant(1)*1e-8;
+
+    ///the array of the state estimations over time
+    DiscreteTimeArray xh;
+    xh.pushBack(xh0,y.getFirstTime()-1);
+
+    ///the reconstruction of the state
+    for (int i=y.getFirstTime();i<=y.getLastTime();++i)
+    {
+        ///introduction of the measurement
+        filter.setMeasurement(y[i],i);
+
+        ///introduction of the input
+        if (i<y.getLastTime())
+            filter.setInput(u[i],i);
+
+        ///get the jacobians by finite differences and provide them to the Kalman filter
+        Matrix a=filter.getAMatrixFD(dx);
+        Matrix c= filter.getCMatrixFD(dx);
+        filter.setA(a);
+        filter.setC(c);
+
+        ///get the estimation and give it to the array
+        Vector xhk=filter.getEstimateState(i);
+
+        ///regulate the part of orientation vector in the state vector
+        xhk.segment(9,3)=tools::regulateOrientationVector(xhk.segment(9,3));
+
+        ///give the new value of the state to the kalman filter.
+        ///This step is usually unnecessary, unless we modify the value of the state esimation
+        ///which is the case here.
+        filter.setState(xhk,i);
+
+        xh.pushBack(xhk,i);
+    }
+
+    return xh;
+}
+
+DiscreteTimeArray imuAttitudeTrajectoryReconstruction(
+    const DiscreteTimeArray & y,
+    const Vector & xh0,
+    const Matrix & p,
+    const Matrix & q,
+    const Matrix & r,
+    double dt)
+{
+    const unsigned inputSize=6;
+
+    ///initialization of a zero input
+    DiscreteTimeArray u;
+    for (int k=y.getFirstTime()-1; k<y.getLastTime(); ++k)
+    {
+        u.pushBack(Vector::Zero(inputSize,1),k);
+    }
+
+    return imuAttitudeTrajectoryReconstruction (y, u, xh0, p, q, r, dt);
+}
+
+
+
+
