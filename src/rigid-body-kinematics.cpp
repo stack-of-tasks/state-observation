79e9bf768fc047512630f79c008537fcc4c710e7
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2013-11-08 16:21:19 +0100

    Factor code

--- src/rigid-body-kinematics.cpp
+++ src/rigid-body-kinematics.cpp
@@ -1,43 +1,29 @@
 #include <state-observation/dynamical-system/algorithm/rigid-body-kinematics.hpp>
+#include <state-observation/tools/miscellaneous-algorithms.hpp>
 
 namespace stateObservation
 {
-    namespace algorithm
+namespace algorithm
 
+{
+    RigidBodyKinematics::~RigidBodyKinematics()
     {
-        RigidBodyKinematics::~RigidBodyKinematics()
-        {
-            //dtor
-        }
+        //dtor
+    }
 
-        void RigidBodyKinematics::integrateKinematics
-                (Vector3 & position, Vector3 & velocity, const Vector3 & acceleration,
+    void RigidBodyKinematics::integrateKinematics
+            (Vector3 & position, Vector3 & velocity, const Vector3 & acceleration,
                     Quaternion & orientation, Vector3 & rotationVelocityVector,
                         const Vector3 & rotationVelocityVectorRate, double dt)
-        {
-            position +=  dt * velocity + 0.5 * dt * dt * acceleration;
-            velocity +=  dt * acceleration;
+    {
+        position +=  dt * velocity + 0.5 * dt * dt * acceleration;
+        velocity +=  dt * acceleration;
 
-            double norm=rotationVelocityVector.norm();
+        orientation = Quaternion( tools::rotationVectorToAngleAxis(rotationVelocityVector) )
+                        * orientation;
 
-            Vector3 a;
+        rotationVelocityVector += dt * rotationVelocityVectorRate;
 
-            if (norm>cst::epsilonAngle)
-                a = rotationVelocityVector / norm;
-            else
-            {
-                a = Vector3::UnitX();
-                norm =0;
-            }
-
-
-
-            AngleAxis rot( norm*dt, a);
-
-            orientation = Quaternion(rot) * orientation;
-
-            rotationVelocityVector += dt * rotationVelocityVectorRate;
-
-        }
     }
+}
 }
