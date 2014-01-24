167c7bcbdc6ea512237705e2f58816ecee6f8c0b
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2014-01-24 19:49:26 +0100

    Add a new test based on finite differences method

---
+++ unit-testings/simple-flex-estimation-test.cpp
@@ -0,0 +1,240 @@
+#include <iostream>
+#include <fstream>
+
+#include <state-observation/noise/gaussian-white-noise.hpp>
+#include <state-observation/examples/offline-ekf-flexibility-estimation.hpp>
+#include <state-observation/dynamical-system/dynamical-system-simulator.hpp>
+#include <state-observation/tools/miscellaneous-algorithms.hpp>
+
+
+using namespace stateObservation;
+
+
+int test()
+{
+    /// The number of samples
+    const unsigned kmax=3000;
+
+    ///sampling period
+    const double dt=5e-3;
+
+    ///Sizes of the states for the state, the measurement, and the input vector
+    const unsigned stateSize=18;
+    const unsigned measurementSize=6;
+    const unsigned inputSize=15;
+
+    ///The array containing all the states, the measurements and the inputs
+    DiscreteTimeArray x;
+    DiscreteTimeArray y;
+    DiscreteTimeArray u;
+    DiscreteTimeArray z;
+
+    DiscreteTimeArray ino;
+    DiscreteTimeArray prediMea;
+
+    ///Contact vector
+    Vector3 contact(Vector3::Random());
+
+    ///Generation
+    {
+        Quaternion q(Vector4::Random());
+        q.normalize();
+        q = Quaternion::Identity();
+        Vector3 odoti(Vector3::Zero());
+        Vector3 oi(Vector3::Zero());
+        Vector3 pos;
+        Vector3 vel;
+        Vector3 acc;
+        kine::fixedPointRotationToTranslation
+                (q.matrix() , oi, odoti,
+                contact, pos, vel, acc);
+        AngleAxis aa(q);
+
+        Vector Xi (Vector::Zero(stateSize,1));
+        Xi.segment(kine::pos,3) = pos;
+        Xi.segment(kine::ori,3) = aa.angle()*aa.axis();
+        Xi.segment(kine::linVel,3) = vel;
+        Xi.segment(kine::angVel,3) = oi;
+        Xi.segment(kine::linAcc,3) = acc;
+        Xi.segment(kine::angAcc,3) = odoti;
+        x.setValue(Xi,0);
+
+
+        Quaternion qCtrl(Vector4::Random());
+        qCtrl.normalize();
+        qCtrl = Quaternion::Identity();
+        Vector3 odotCtrl(Vector3::Zero());
+        Vector3 oCtrl(Vector3::Zero());
+        Vector3 posCtrl(Vector3::Zero());
+        Vector3 velCtrl(Vector3::Zero());
+        Vector3 accCtrl(Vector3::Zero());
+
+        Quaternion qImu(q*qCtrl);
+        Vector3 oImu(Vector3::Zero());
+        Vector3 posImu(q.matrix()*posCtrl+pos);
+        Vector3 velImu(Vector3::Zero());
+        Vector3 accImu(Vector3::Zero());
+
+
+
+        AccelerometerGyrometer imu;
+
+
+
+
+
+        for (unsigned i=1; i<kmax; ++i)
+        {
+            q = kine::rotationVectorToAngleAxis(oi*dt)*q;
+            aa=q;
+            oi+=odoti*dt;
+            odoti << 0.1*sin(0.007*i),  0.2*sin(0.03*i),  0.25*sin(0.02*i);
+            odoti *= 5;
+            kine::fixedPointRotationToTranslation(q.matrix() , oi, odoti,
+                                                        contact, pos, vel, acc);
+
+
+            Xi.segment(kine::pos,3) = pos;
+            Xi.segment(kine::ori,3) = aa.angle()*aa.axis();
+            Xi.segment(kine::linVel,3) = vel;
+            Xi.segment(kine::angVel,3) = oi;
+            Xi.segment(kine::linAcc,3) = acc;
+            Xi.segment(kine::angAcc,3) = odoti;
+
+            x.setValue(Xi,i);
+
+            qCtrl = kine::rotationVectorToAngleAxis(oCtrl*dt)*qCtrl;
+            AngleAxis aaCtrl(qCtrl);
+            oCtrl+=odotCtrl*dt;
+            odotCtrl << 0.15*sin(0.008*i), 0.1*sin(0.023*i), 0.2*sin(0.025*i);
+            posCtrl += velCtrl*dt;
+            velCtrl += accCtrl*dt;
+            accCtrl << 0.12*sin(0.018*i), 0.08*sin(0.035*i), 0.3*sin(0.027*i);
+
+            Vector Ui (Vector::Zero(inputSize,1));
+            Ui.segment(kine::pos,3) = posCtrl;
+            Ui.segment(kine::ori,3) = aaCtrl.angle()*aaCtrl.axis();
+            Ui.segment(kine::linVel,3) = velCtrl;
+            Ui.segment(kine::angVel,3) = oCtrl;
+            Ui.segment(kine::linAcc,3) = accCtrl;
+            u.setValue(Ui,i);
+
+            Quaternion newqImu(q*qCtrl);
+            Vector3 newPosImu(q.matrix()*posCtrl+pos);
+            Vector3 newVelImu(tools::derivate(posImu,newPosImu,dt));
+
+            accImu = tools::derivate(velImu,newVelImu,dt);
+            velImu = newVelImu;
+            posImu = newPosImu;
+
+            oImu = kine::derivateRotationFD(qImu, newqImu, dt);
+            qImu = newqImu;
+
+            Vector Ximu(Vector::Zero(10,1));
+            Ximu[0]=qImu.w();
+            Ximu[1]=qImu.x();
+            Ximu[2]=qImu.y();
+            Ximu[3]=qImu.z();
+            Ximu.segment(4,3)=accImu;
+            Ximu.segment(7,3)=oImu;
+
+            imu.setState(Ximu,i);
+            z.setValue(Ximu,i);
+            y.setValue(imu.getMeasurements(),i);
+
+        }
+    }
+
+    ///the initalization of a random estimation of the initial state
+    Vector xh0=Vector::Zero(stateSize,1);
+
+    std::vector<Vector3> contactPositions;
+
+    contactPositions.push_back(contact);
+
+
+    stateObservation::DiscreteTimeArray xh=
+        stateObservation::examples::offlineEKFFlexibilityEstimation
+            (y,u,xh0,1,contactPositions,dt,&ino, &prediMea);
+
+    ///file of output
+    std::ofstream f[10];
+    f[0].open("realState.dat");
+    f[1].open("estimatedState.dat");
+    f[2].open("gravity.dat");
+    f[3].open("estimatedGravity.dat");
+    f[4].open("error.dat");
+    f[5].open("measurement.dat");
+    f[6].open("predictedMeasurement.dat");
+    f[7].open("inovation.dat");
+    f[8].open("stateImu.dat");
+    f[9].open("input.dat");
+
+    double error;
+
+    ///the reconstruction of the state
+    for (int i=y.getFirstTime();i<=y.getLastTime();++i)
+    {
+        ///display part, useless
+        Vector3 g;
+        {
+            Matrix3 R;
+            Vector3 orientationV=Vector(x[i]).segment(kine::ori,3);
+            double angle=orientationV.norm();
+            if (angle > cst::epsilonAngle)
+                R = AngleAxis(angle, orientationV/angle).toRotationMatrix();
+            else
+                R = Matrix3::Identity();
+            g=R.transpose()*Vector3::UnitZ();
+            g.normalize();
+        }
+
+        Vector3 gh;
+        {
+            Matrix3 Rh;
+
+            Vector3 orientationV=Vector(xh[i]).segment(kine::ori,3);
+            double angle=orientationV.norm();
+            if (angle > cst::epsilonAngle)
+                Rh = AngleAxis(angle, orientationV/angle).toRotationMatrix();
+            else
+                Rh = Matrix3::Identity();
+            gh=Rh.transpose()*Vector3::UnitZ();
+            gh.normalize();
+        }
+
+        error = acos(double(g.transpose()*gh)) * 180 / M_PI;
+
+        f[0]<< i<< " \t "<< x[i].transpose()<< std::endl;
+        f[1]<< i<< " \t "<< xh[i].transpose()<< std::endl;
+        f[2]<< i<< " \t "<< g.transpose()<< std::endl;
+        f[3]<< i<< " \t "<< gh.transpose()<< std::endl;
+        f[4]<< i<< " \t "<< error << std::endl;
+        f[5]<< i<< " \t "<< y[i].transpose()<< std::endl;
+        f[6]<< i<< " \t "<< prediMea[i].transpose()<< std::endl;
+        f[7]<< i<< " \t "<< ino[i].transpose()<< std::endl;
+        f[8]<< i<< " \t "<< z[i].transpose()<< std::endl;
+        f[9]<< i<< " \t "<< u[i].transpose()<< std::endl;
+    }
+
+    std::cout << "Error " << error << ", test: " ;
+
+    if (error > 2)
+    {
+        std::cout << "FAILED !!!!!!!";
+        return 1;
+    }
+    else
+    {
+        std::cout << "SUCCEEDED !!!!!!!";
+        return 0;
+    }
+}
+
+int main()
+{
+
+
+    return test();
+
+}
